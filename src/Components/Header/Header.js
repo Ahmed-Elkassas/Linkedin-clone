45bd17cdd1ab144327aba04d9cbe465c47fd3c78
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-03-12 20:31:51 +0200

    Add styled nav-bar to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,9 +14,43 @@ const Header = () => {
             <input type="text" placeholder="Search" />
           </div>
           <SearchIcon>
-            <img src="/images/search-icon.svg" alt="" />
+            <img src="/images/search-icon.svg" alt="search" />
           </SearchIcon>
         </Search>
+        <Nav>
+          <NavContainer>
+            <NavList>
+              <a href="/">
+                <img src="/images/nav-home.svg" alt="home" />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="/">
+                <img src="/images/nav-network.svg" alt="home" />
+                <span>My Network</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="/">
+                <img src="/images/nav-jobs.svg" alt="home" />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="/">
+                <img src="/images/nav-messaging.svg" alt="home" />
+                <span>messaging</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="/">
+                <img src="/images/nav-home.svg" alt="home" />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+          </NavContainer>
+        </Nav>
       </Content>
     </Container>
   );
@@ -38,6 +72,7 @@ const Content = styled.div`
   margin: 0 auto;
   min-height: 100%;
   max-width: 1128px;
+  /* padding: 5px 0; */
 `;
 
 const Logo = styled.span`
@@ -78,6 +113,54 @@ const SearchIcon = styled.div`
   width: 40px;
   pointer-events: none;
   margin: 0;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background-color: white;
+    width: 100%;
+  }
+`;
+const NavContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: nowrap;
+`;
+const NavList = styled.li`
+  display: flex;
+  align-items: center;
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    font-size: 12px;
+    min-height: 52px;
+    min-width: 88px;
+    position: relative;
+  }
+  span {
+    color: rgba(0, 0, 0, 0.6);
+    display: flex;
+    align-items: center;
+  }
+  @media (max-width: 768px) {
+    min-width: 70px;
+  }
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
 `;
 
 export default Header;
